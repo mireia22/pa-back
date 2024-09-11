d452dd4a2ee871b2dd35f03a1b9190ea40198e4f
mireia22 <mireiagarciaferrer22@gmail.com>
2024-09-11 18:36:30 +0200

    add 3001 port to listen

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=["http://localhost:3000", "http://localhost:3001"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
